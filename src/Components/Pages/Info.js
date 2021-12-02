60c1c6abc71e9c24233cca2694cb624bdb730475
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-12-01 23:27:28 -0800

    Add linear transition to page routes

--- src/Components/Pages/Info.js
+++ src/Components/Pages/Info.js
@@ -1,24 +1,20 @@
 import { motion } from 'framer-motion';
 
-const pageVariants = {
-  initial: {
-    opacity: 0,
-  },
-  in: {
-    opacity: 1,
-  },
-  out: {
-    opacity: 0,
-  },
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
 };
 
 export default function Info() {
   return (
     <motion.div
-      initial='initial'
-      animate='in'
-      exit='out'
-      variants={pageVariants}
+      variants={variants} // Pass the variant object into Framer Motion
+      initial='hidden' // Set the initial state to variants.hidden
+      animate='enter' // Animated state to variants.enter
+      exit='exit' // Exit state (used later) to variants.exit
+      transition={{ type: 'linear' }} // Set the transition to linear
+      className=''
     >
       <div>Info</div>
     </motion.div>
--- src/Components/Pages/Work.js
+++ src/Components/Pages/Work.js
@@ -1,30 +1,20 @@
 import { motion } from 'framer-motion';
 
-const pageVariants = {
-  in: {
-    opacity: 1,
-    x: 0,
-  },
-  out: {
-    opacity: 0,
-    x: '-100vw',
-  },
-};
-
-const pageTransition = {
-  type: 'tween',
-  ease: 'circInOut',
-  duration: 3,
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
 };
 
 function Work() {
   return (
     <motion.div
-      initial='initial'
-      animate='in'
-      exit='out'
-      variants={pageVariants}
-      transition={pageTransition}
+      variants={variants} // Pass the variant object into Framer Motion
+      initial='hidden' // Set the initial state to variants.hidden
+      animate='enter' // Animated state to variants.enter
+      exit='exit' // Exit state (used later) to variants.exit
+      transition={{ type: 'linear' }} // Set the transition to linear
+      className=''
     >
       <div>Work</div>
     </motion.div>
