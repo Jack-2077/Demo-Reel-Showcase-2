65c864d5cac02c77067cd23f8126dfd0d83c11bf
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-12-06 21:21:03 -0800

    Remove comments

--- src/Components/Reels/Reel.js
+++ src/Components/Reels/Reel.js
@@ -46,34 +46,3 @@ export default function Reel() {
     </div>
   );
 }
-
-/*
-<div
-        class='sqs-block video-block sqs-block-video'
-        data-block-json='{"blockAnimation":"none","layout":"caption-hidden","overlay":false,"description":{"html":""},"html":"<iframe src=\"https://player.vimeo.com/video/496218444?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0\" width=\"1920\" height=\"1080\" frameborder=\"0\" allow=\"autoplay; fullscreen\" allowfullscreen></iframe>","resolvedBy":"manual"}'
-        data-block-type='32'
-        id='block-40f2c1d25036c5d4c644'
-      >
-        <div class='sqs-block-content' id='yui_3_17_2_1_1638766030293_93'>
-          <div
-            class='sqs-video-wrapper video-none'
-            data-html='<iframe src="https://player.vimeo.com/video/496218444?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>'
-            data-provider-name=''
-            id='yui_3_17_2_1_1638766030293_100'
-          >
-            <div class='intrinsic'>
-              <div class='intrinsic-inner' style='padding-bottom: 56.25%;'>
-                <iframe
-                  src='https://player.vimeo.com/video/496218444?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0'
-                  width='1920'
-                  height='1080'
-                  frameborder='0'
-                  allow='autoplay; fullscreen'
-                  allowfullscreen=''
-                  id='yui_3_17_2_1_1638766030293_104'
-                ></iframe>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>*/
