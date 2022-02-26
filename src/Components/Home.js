a7b1cec1a639c5dd58d129d11242866d6c914dca
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-26 14:51:44 -0800

    Add margintop for home images

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -39,70 +39,92 @@ function Home() {
       <div>
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={0.5}>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[0]} />
-              <div class='middle'>
-                <div class='text'>GASSTATION</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[0]} />
+                <div class='middle'>
+                  <div class='text'>GASSTATION</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[2]} />
-              <div class='middle'>
-                <div class='text'>917</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[2]} />
+                <div class='middle'>
+                  <div class='text'>917</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[10]} />
-              <div class='middle'>
-                <div class='text'>CRITTER</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[10]} />
+                <div class='middle'>
+                  <div class='text'>CRITTER</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[1]} />
-              <div class='middle'>
-                <div class='text'>ATELIER</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[1]} />
+                <div class='middle'>
+                  <div class='text'>ATELIER</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[5]} />
-              <div class='middle'>
-                <div class='text'>HALLOWEEN</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[5]} />
+                <div class='middle'>
+                  <div class='text'>HALLOWEEN</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={6} className='container'>
-              <ReelGrid imageArray={MAIN_SET[6]} />
-              <div class='middle'>
-                <div class='text2'>PORSCHE SINGER</div>
+            <Grid item xs={6}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[6]} />
+                <div class='middle'>
+                  <div class='text2'>PORSCHE SINGER</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={6} className='container'>
-              <ReelGrid imageArray={MAIN_SET[8]} />
-              <div class='middle'>
-                <div class='text2'>RESTLESS</div>
+            <Grid item xs={6}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[8]} />
+                <div class='middle'>
+                  <div class='text2'>RESTLESS</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[3]} />
-              <div class='middle'>
-                <div class='text'>VELAR</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[3]} />
+                <div class='middle'>
+                  <div class='text'>VELAR</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={6} className='container'>
-              <ReelGrid imageArray={MAIN_SET[7]} />
-              <div class='middle'>
-                <div class='text2'>ECDYSIS721</div>
+            <Grid item xs={6}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[7]} />
+                <div class='middle'>
+                  <div class='text2'>ECDYSIS721</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={6} className='container'>
-              <ReelGrid imageArray={MAIN_SET[4]} />
-              <div class='middle'>
-                <div class='text2'>ROVER</div>
+            <Grid item xs={6}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[4]} />
+                <div class='middle'>
+                  <div class='text2'>ROVER</div>
+                </div>
               </div>
             </Grid>
-            <Grid item xs={12} className='container'>
-              <ReelGrid imageArray={MAIN_SET[9]} />
-              <div class='middle'>
-                <div class='text'>TOWNCAR</div>
+            <Grid item xs={12}>
+              <div className='container'>
+                <ReelGrid imageArray={MAIN_SET[9]} />
+                <div class='middle'>
+                  <div class='text'>TOWNCAR</div>
+                </div>
               </div>
             </Grid>
           </Grid>
